1de0793d67e88f0f425d17bf41495b0ef2214653
chadeqmartin <chadeqmartin@gmail.com>
2023-10-13 13:12:19 -0400

    updated with working solution

--- runner.py
+++ runner.py
@@ -1,5 +1,37 @@
-from school import School 
+from classes.school import School 
 
 school = School('Ridgemont High') 
 
-print(school.name)
+while True:
+    choice = input("""
+What would you like to do?
+Options:
+    1. List All Students
+    2. View Individual Student <student_id>
+    3. Add a Student
+    4. Remove a Student <student_id>
+    5. Quit
+""")
+    if choice == "1":
+        school.list_students()
+    elif choice == '2':
+        student_id = input('Enter student id:')
+        student = school.find_student_by_id(student_id)
+        if student:
+            print(student)
+    elif choice == '3':  
+        student_data = {'role':'student'}
+        student_data['name']      = input('Enter student name:\n')
+        student_data['age']       = input('Enter student age: \n')
+        student_data['school_id'] = input('Enter student school id: \n')
+        student_data['password']  = input('Enter student password: \n')
+        school.add_student(student_data)
+    elif choice == "4":
+        student_id = input('Enter student id:')
+        student = school.find_student_by_id(student_id)
+        if student:
+            school.students.remove(student)
+            print("Student was deleted")
+    if choice == "5":
+        print('goodbye')
+        break
